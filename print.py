bf3423b4eaf8472b7364327ebb1941562bb63e63
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-06 17:12:10 +0300

    Create print.py

---
+++ print.py
@@ -0,0 +1,11 @@
+print("merhaba dunya")
+print('merhaba dunya')
+
+
+
+#print('bu sene linux'a gectik') hatalı kullanım 
+print('bu sene linux\'a gectik') #doğru kullanım
+print("bu sene linux\'a gectik") #doğru kullanım
+
+#print("hoca, "kusadasına gidiyorum" demiş") #hatalı kullanım
+print("hoca, \"kusadasına gidiyorum\" demiş") #doğru kullanım
