0ee3490eb273e49351a28b522ba1249fa626730a
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-06 17:12:21 +0300

    Create VeriGirisi.py

---
+++ VeriGirisi.py
@@ -0,0 +1,35 @@
+#  ad=input("lutfen adinizi giriniz : ")
+#  print("isim girildi :  %s" %(ad))
+
+# sayi1=input("1. sayiyi giriniz :")
+# sayi2=input("2. sayiyi giriniz :")
+# print(sayi1+sayi2) -------------------------#string olarak yazar
+
+# sayi7=input("1. sayiyi giriniz :")
+# sayi8=input("2. sayiyi giriniz :")
+# print(int(sayi7+sayi8))------------------ #string olarak hesaplar
+
+# sayi3=input("1. sayiyi giriniz :")
+# sayi4=input("2. sayiyi giriniz :")
+# print(int(sayi3)+int(sayi4)) -------------------#integer olarak hesaplar
+
+#  sayi5=int(input("1. sayiyi giriniz :"))
+#  sayi6=int(input("2. sayiyi giriniz :"))
+#  print(sayi5+sayi6)  ----------------#integer olarak hesaplar
+
+
+# yil=input("dogum yilinizi giriniz: ")----------------  HATALI KULLANIM
+# yas=2021-yil 
+# print("yasiniz = %s" %yas)
+
+yil=input("dogum yilinizi giriniz: ")
+yas=2021-int(yil)
+print("yasiniz = %s" %yas)
+
+
+
+
+
+
+
+
