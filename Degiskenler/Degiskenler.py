edfe7360474aff0e17beba7354bb4d565db90abc
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-07 13:00:37 +0300

    Create Degiskenler.py

---
+++ Degiskenler/Degiskenler.py
@@ -0,0 +1,5 @@
+elma=25
+miktar=5
+print("elmanın kilosu",elma,"lira ise",miktar,"kilo elma alırsak",elma*miktar, "tl öderiz")
+print("elmanın kilosu %s lira ise %s kilo elma alırsak %s tl öderiz" %(elma,miktar,elma*miktar))
+print(" %s %s %s"  %(elma-miktar,elma/miktar,elma%miktar)  )
