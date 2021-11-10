222abd0f643109b0864fe938742a4ee82dfc95a2
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-10 21:50:23 +0300

    Create İf_4.py

---
+++ Kosul İfadeleri/İf_4.py
@@ -0,0 +1,7 @@
+sayi=int(input("bir sayi giriniz: "))
+if sayi>0:
+    print("sayi poziftir")
+else:
+        print("sayi negatiftir") #-------else blogunun icinde
+    #print("sayi negatiftir")-----------hatalı kullanım
+# print("sayi negatiftir")----------if else blogunun dısında
