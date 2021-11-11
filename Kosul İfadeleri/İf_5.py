6cd3acbb1a934ee3e638c21352a484512a441d2d
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-11 19:31:36 +0300

    Create İf_5.py

---
+++ Kosul İfadeleri/İf_5.py
@@ -0,0 +1,10 @@
+sayi=int(input("bir sayi giriniz: "))
+if sayi>50 and sayi%2==0:
+    print("sayi 50 den büyük ve cift")
+elif sayi>50:
+     print("sayi 50 den buyuk ve tek")
+     
+elif sayi<50 and sayi%2==0:
+     print("sayi 50 den kucuk ve cift")
+else:
+     print("sayi 50 den kucuk ve tek")
