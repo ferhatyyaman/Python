1a0709fc47caeac337a02ec90aa80082da87992b
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-14 10:30:31 +0300

    Create For_1.py

---
+++ For Dongusu/For_1.py
@@ -0,0 +1,8 @@
+# for i in range(1,10):   #---------1-9 a kadar sayilari yazdirir
+#     print(i)
+
+# for i in range(5,23,3): #---------5 ten 23 e kadar 3 er 3 er yazdırır
+#      print(i)
+
+for harf in "MARMARA":    #---------marmara harflerini yazdirir
+    print(harf)
