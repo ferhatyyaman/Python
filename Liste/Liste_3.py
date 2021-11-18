648e912c2a64ac46dda86fcc23696f4e7d3a80fd
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-18 20:47:35 +0300

    Create Liste_3.py

---
+++ Liste/Liste_3.py
@@ -0,0 +1,10 @@
+# Kullanıcıdan sırasıyla degerleri alarak bir listeye ekleyen kodun yazımı
+ad=input("adınızı giriniz :")
+soyad=input("soyadınızı giriniz :")
+cadde=input("caddenizi giriniz :")
+ilce=input("ilçenizi giriniz :")
+il=input("ilinizi giriniz :")
+ulke=input("ulke verinizi giriniz :")
+
+Liste=[ad,soyad,cadde,ilce,il,ulke]
+print(Liste)
