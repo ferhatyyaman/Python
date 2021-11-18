195144958ff6405c65884007a069fde3032bf33d
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-18 20:47:39 +0300

    Create Liste_4.py

---
+++ Liste/Liste_4.py
@@ -0,0 +1,7 @@
+#Bir listeyi genişletmek amacıyla kullanılır
+Liste1=["İstanbul","ankara","izmir","bursa"]
+Liste2=["düzce","adana","isparta","adıyaman"]
+
+Liste1.extend(Liste2)  #liste1 e liste2 yi ekliyoruz
+#Liste1=Liste1+Liste2 aynı görevi görür
+print(Liste1)
