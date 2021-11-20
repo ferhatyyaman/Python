d6d9f9d25fe517d58eb2d2e1bc6104747a412327
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-20 20:33:35 +0300

    Create Liste_5.py

---
+++ Liste/Liste_5.py
@@ -0,0 +1,12 @@
+#Bir listeden elemanı kendisini belirterek çıkarmak için remove()
+
+Liste=["İstanbul","ankara","izmir","bursa"]
+Liste.remove("İstanbul") #istanbulu cıkardık
+print(Liste)
+
+
+#Bir listeden in belirterek çıkarmak için remove()
+
+Liste=["İstanbul","ankara","izmir","bursa"]
+Liste.pop(1) #ankarayı cıkardık
+print(Liste)
