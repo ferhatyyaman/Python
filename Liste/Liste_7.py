822eb0e60d2605fbfaba32b8758cba9098efdcd8
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-11-24 09:44:56 +0300

    Create Liste_7.py

---
+++ Liste/Liste_7.py
@@ -0,0 +1,19 @@
+Sehirler =["Çanakkale","İzmir","Aydın","Muğla","Antalya","Mersin","Adana"]
+print(Sehirler[-1])
+print(Sehirler[2:4])
+print(Sehirler[:3])
+print(Sehirler[2:])
+print(Sehirler[0:3])
+
+
+Sehirler[7:7]=["hatay","ankara"] # sona ekleme yaptık
+print(Sehirler[7:])
+
+Sehirler[3:3]=["mus","agrı"] #araya ekleme yapar
+print(Sehirler[:])
+
+
+
+
+
+
