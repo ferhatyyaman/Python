b37a3c63180c51a8dc8438032b1c3f11f244443b
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-12-05 16:11:23 +0300

    Create return.py

---
+++ Fonksiyonlar/return.py
@@ -0,0 +1,8 @@
+#Python’da her fonksiyonun bir “dönüş değeri” vardır Yani Python’daki bütün fonksiyonlar bir değer döndürür
+
+
+def fonk():
+    a=5
+    print(a)
+
+print(fonk()) #return olmadıgı icin none diye deger dondurur
