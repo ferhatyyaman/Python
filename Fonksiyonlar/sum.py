7be8d0e9100fc1b0aa041d97128911f8ff1b84d9
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-12-06 20:57:12 +0300

    Create sum.py

---
+++ Fonksiyonlar/sum.py
@@ -0,0 +1,20 @@
+# sayilar=[45,90,43]
+# print(sum(sayilar))
+
+
+# sayilar1=[3,4,5]
+# a=1
+# for i in sayilar1:
+#     a=a*i
+#     print(a)
+
+
+
+def carp(liste):
+    a=1
+    for i in liste:
+        a=a*i
+        print(a)
+
+sayilar2=[2,5,9]
+carp(sayilar2)
