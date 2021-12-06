f3f819c9b1037d81628da93cdc0dddc3bafd797e
ferhatyyaman <66822481+ferhatyyaman@users.noreply.github.com>
2021-12-06 20:57:08 +0300

    Create pass.py

---
+++ Fonksiyonlar/pass.py
@@ -0,0 +1,11 @@
+def deneme():
+    liste=[]
+    while True:
+        
+        a = input("giriniz: ")
+        if a == "0":
+            pass
+        else:
+            liste.append(a)
+            print(liste)
+deneme()
